3494a82791d6e748d720669435d2b915386d6f6f
SuperCoder <propscode254@gmail.com>
2024-01-18 18:44:18 +0100

    middelware chektoken done

--- backend_cms_doc_time/src/middlewares/auth.middleware.js
+++ backend_cms_doc_time/src/middlewares/auth.middleware.js
@@ -3,7 +3,8 @@ import { verifyToken } from "../auth/auth.service.js";
 export const checkToken = (req, res, next) => {
   const token = req.cookies.doctor_cms_auth;
   try {
-    const payload = verifyToken(token);
+    // We pass the payload, we may need it later in the route, so we add it to the req that goes further in the function
+    req.doctor_cms_auth = verifyToken(token);
     next();
   } catch (error) {
     console.log(error.message);
